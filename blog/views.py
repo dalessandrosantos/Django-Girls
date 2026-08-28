e67229f74d5c9f913bcbcab3732b3118aaf5f440
Dalessandro Santos <dalessandrosantos83@gmail.com>
2026-08-27 22:17:47 -0300

    Trazendo dados do backend para o frontend

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone # Importa recursos para trabalhar com data e hora
 
-def post_view(request):
-    return render(request, 'blog/post_list.html')
+def post_view(request):   
+    
+    # Busca os posts já publicados (data <= agora), ordenados da mais antiga para a mais recente
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  
+
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/post_list.html', context)
