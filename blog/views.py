67d35164a348294071cf7e841598c9a8378c45f3
Dalessandro Santos <dalessandrosantos83@gmail.com>
2026-08-29 20:07:06 -0300

    Adiciona formulário no blog

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone # Importa recursos para trabalhar com data e hora
+from .forms import PostForm
 
 def post_view(request):   
     
     # Busca os posts já publicados (data <= agora), ordenados da mais antiga para a mais recente
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  
 
-    context = {
-        'posts': posts
-    }
+    context = {'posts': posts}
     return render(request, 'blog/post_list.html', context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    context = {'post': post}
+    return render(request, 'blog/post_detail.html', context)
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list') 
+    else:
+        form = PostForm()
+
+    context = {'form': form}
+    return render(request, 'blog/post_edit.html', context)
